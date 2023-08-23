afce01904836f64cf279138b9e148bf06020ac05
DefySkyZ <Demoursky@gmail.com>
2023-08-23 17:10:59 +0800

    Part 7 of Project - created function parseData Parse the data to retain the necessary 10 years period, Split the critical information to different columns, Retain necessary regions only, as well as called out parseData function. Line 14 still has error

--- main.py
+++ main.py
@@ -9,7 +9,36 @@ class VistorsAnalyticsUtils:
     def loadDataFile(self):
         data = self.data
         return data
-
+    def parseData(self, year_choice, region_choice):
+        # Filter data based on year_choice and region_choice
+        filtered_data = self.data[(self.data['Year_Period'] == year_choice) & (self.data['Region'] == region_choice)]
+        
+        # Parse the data to retain the necessary 10 years period
+        if year_choice == 1:
+            start_year = 1978
+        elif year_choice == 2:
+            start_year = 1988
+        elif year_choice == 3:
+            start_year = 1998
+        elif year_choice == 4:
+            start_year = 2008
+        end_year = start_year + 9  # 10 years period
+        
+        filtered_data = filtered_data[(filtered_data['Year'] >= start_year) & (filtered_data['Year'] <= end_year)]
+        
+        # Split the critical information to different columns
+        filtered_data['Year_Start'] = start_year
+        filtered_data['Year_End'] = end_year
+        
+        # Retain necessary regions only
+        if region_choice == 1:
+            filtered_data = filtered_data.iloc[:, 1:19]  # Asia
+        elif region_choice == 2:
+            filtered_data = filtered_data.iloc[:, 19:30]  # Europe
+        elif region_choice == 3:
+            filtered_data = filtered_data.iloc[:, 30:]  # Others
+        
+        return filtered_data
 
 def main():
     while True:
@@ -35,12 +64,16 @@ def main():
     
 #Made it easier to read this way
 loadDataFile = VistorsAnalyticsUtils().loadDataFile()
+parsed_data = VistorsAnalyticsUtils().parseData(year_choice, region_choice)
+    
+print("Parsed Data:")
+print(parsed_data)
 
 if __name__ == '__main__':
     main()
     print(loadDataFile.head())
-    print(loadDataFile.iloc[:,1:19]) #Asia
-    print(loadDataFile.iloc[:,19:30]) #Europe
-    print(loadDataFile.iloc[:, 30:]) #Others
-
+    ##print(loadDataFile.iloc[:,1:19]) #Asia
+    ##print(loadDataFile.iloc[:,19:30]) #Europe
+    ##print(loadDataFile.iloc[:, 30:]) #Others
+    
 
