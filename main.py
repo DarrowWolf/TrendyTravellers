03c533b33785e42d87ee9c96b8c6bb9c5286c340
Darrow Wolf <47889966+DarrowWolf@users.noreply.github.com>
2023-08-24 22:28:13 +0800

    feat: loadDataFile and parseData
    Closes #4.

--- main.py
+++ main.py
@@ -1,79 +1,91 @@
 import pandas as pd
 
-year_choice = 0
-region_choice = 0
-
-class VistorsAnalyticsUtils:
-    data = pd.read_csv('Int_Monthly_Visitor.csv') # Made data global variable within the class
-    
+class VisitorsAnalyticsUtils:
     def loadDataFile(self):
-        data = self.data
+        data = pd.read_csv('Int_Monthly_Visitor.csv', na_values=[' na ']) #Opens csv file and declare ' na ' under na_value
+        
+        data.columns = [col.strip() for col in data.columns]  #Changes all object dtypes to float
+        for col in data.columns[1:]:
+            if data[col].dtype == "object":
+                data[col] = data[col].str.strip().str.replace(',', '').astype(float)
+        
+        data = data.fillna(0.0) #Replaces all the 'na' with 0.0
+        data.rename(columns={'Unnamed: 0': ''}, inplace=True) #Get rid of first column name
+        
+        data[''] = data[''].str.strip() #trims data
+    
+        numerical_columns = data.columns[1:] #Getting all numerical columns after first column
+        data[numerical_columns] = data[numerical_columns].apply(pd.to_numeric, errors='coerce') # make everything numeric
+        
+        data.set_index('', inplace=True) # set first column as the index
+        
+        print("*** first 5 rows of data loaded ***")
+        print(data.head())
+        
         return data
+        
     def parseData(self, year_choice, region_choice):
-        # Filter data based on year_choice and region_choice
-        filtered_data = self.data[(self.data['Year_Period'] == year_choice) & (self.data['Region'] == region_choice)]
+        data = self.loadDataFile()
         
-        # Parse the data to retain the necessary 10 years period
-        if year_choice == 1:
-            start_year = 1978
-        elif year_choice == 2:
-            start_year = 1988
-        elif year_choice == 3:
-            start_year = 1998
-        elif year_choice == 4:
-            start_year = 2008
-        end_year = start_year + 9  # 10 years period
+        regions = {
+            1: ['Brunei Darussalam', 'Indonesia', 'Malaysia', 'Philippines', 'Thailand', 
+                'Viet Nam', 'Myanmar', 'Japan', 'Hong Kong', 'China', 'Taiwan', 
+                'Korea, Republic Of', 'India', 'Pakistan', 'Sri Lanka', 'Saudi Arabia', 
+                'Kuwait', 'UAE'],
+            2: ['United Kingdom', 'Germany', 'France', 'Italy', 'Netherlands', 
+                'Greece', 'Belgium & Luxembourg', 'Switzerland', 'Austria', 'Scandinavia', 
+                'CIS & Eastern Europe'],
+            3: ['USA', 'Canada', 'Australia', 'New Zealand', 'Africa']
+        }
         
-        filtered_data = filtered_data[(filtered_data['Year'] >= start_year) & (filtered_data['Year'] <= end_year)]
+        selected_region = regions[region_choice]
         
-        # Split the critical information to different columns
-        filtered_data['Year_Start'] = start_year
-        filtered_data['Year_End'] = end_year
+        periods = {
+            1: (1978, 1987),
+            2: (1988, 1997),
+            3: (1998, 2007),
+            4: (2008, 2017)
+        }
         
-        # Retain necessary regions only
-        if region_choice == 1:
-            filtered_data = filtered_data.iloc[:, 1:19]  # Asia
-        elif region_choice == 2:
-            filtered_data = filtered_data.iloc[:, 19:30]  # Europe
-        elif region_choice == 3:
-            filtered_data = filtered_data.iloc[:, 30:]  # Others
+        start_year, end_year = periods[year_choice]
         
-        return filtered_data
+        data['year'] = data.index.to_series().apply(lambda x: int(x.split()[0])) #exract year from first column
+        
+        data = data[(data['year'] >= start_year) & (data['year'] <= end_year)]
+        data = data[selected_region + ['year']]
+        
+        for column in selected_region:
+            data[column] = data[column].astype('int64')
+        
+        print("\n*** Parsed Data (Regions) ***")
+        print(data.info())
+        
+        print("\n*** Parsed Data (Years) ***")
+        print(data['year'].describe())
+        
+        return data
 
 def main():
+    year_choice = 0
+    region_choice = 0
     while True:
         try:
             year_choice = int(input("Enter year period (1: 1978-1987, 2: 1988-1997, 3: 1998-2007, 4: 2008-2017): "))
             if year_choice not in {1, 2, 3, 4}: # Checks if year_choice input is not 1, 2, 3, 4
-                print("Invalid year choice, please choose the number representing the year period!")
-                year_choice = 0 # Assign year_choice back to 0 because from the input amove it already assigns a value.
-                continue
-            elif year_choice in {1, 2, 3, 4}: # Checks if year_choice input is 1, 2, 3, 4
-                region_choice = int(input("Enter region (1: Asia, 2: Europe, 3: Others): "))
-                if region_choice not in {1, 2, 3}:  # If the year_choice input is 1, 2, 3, 4. It will go to this line to check if the input is not 1, 2, 3
-                    print("Invalid region choice, please choose the number representing the region!")
-                    region_choice = 0 # Does the same thing as above is it is not the right input
-                    continue
-                print(year_choice, region_choice) # Debugging to see if what you input in, is correct
-                break
+                raise ValueError("Invalid year choice, please choose the number representing the year period!")
 
-        except:
-            print("Invalid input")
+            region_choice = int(input("Enter region (1: Asia, 2: Europe, 3: Others): "))
+            if region_choice not in {1, 2, 3}:  # If the year_choice input is 1, 2, 3, 4. It will go to this line to check if the input is not 1, 2, 3
+                raise ValueError("Invalid region choice, please choose the number representing the region!")
+
+            break
+        except ValueError as ve:
+            print(ve)
             continue
 
-    
-#Made it easier to read this way
-loadDataFile = VistorsAnalyticsUtils().loadDataFile()
-parsed_data = VistorsAnalyticsUtils().parseData(year_choice, region_choice)
-    
-print("Parsed Data:")
-print(parsed_data)
+    # Made it easier to read this way
+    utils = VisitorsAnalyticsUtils()
+    parsed_data = utils.parseData(year_choice, region_choice)
 
 if __name__ == '__main__':
     main()
-    print(loadDataFile.head())
-    ##print(loadDataFile.iloc[:,1:19]) #Asia
-    ##print(loadDataFile.iloc[:,19:30]) #Europe
-    ##print(loadDataFile.iloc[:, 30:]) #Others
-    
-
