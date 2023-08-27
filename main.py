4a536bd7f6cc2d1ea300e26900e9eef0756d4011
Darrow Wolf <47889966+DarrowWolf@users.noreply.github.com>
2023-08-28 02:33:44 +0800

    chore: refactor code
    added extra comments as well to understand code better

--- main.py
+++ main.py
@@ -13,28 +13,29 @@ class VisitorsAnalyticsUtils:
         }
     
     def loadDataFile(self):
-        data = pd.read_csv('Int_Monthly_Visitor.csv', na_values=[' na ']) #Opens csv file and declare ' na ' under na_value
+        data = pd.read_csv('Int_Monthly_Visitor.csv', na_values=[' na ']) # Opens csv file and declare ' na ' under na_value
         
-        data.columns = [col.strip() for col in data.columns]  #Changes all object dtypes to float
+        data.columns = [col.strip() for col in data.columns]  # Changes all object dtypes to float
         for col in data.columns[1:]:
             if data[col].dtype == "object":
                 data[col] = data[col].str.strip().str.replace(',', '').astype(float)
         
-        data = data.fillna(0.0) #Replaces all the 'na' with 0.0
-        data.rename(columns={'Unnamed: 0': ''}, inplace=True) #Get rid of first column name
+        data = data.fillna(0.0) # Replaces all the 'na' with 0.0
+        data.rename(columns={'Unnamed: 0': ''}, inplace=True) # Get rid of first column name
         
-        data[''] = data[''].str.strip() #trims data
+        data[''] = data[''].str.strip() # trims data
     
-        numerical_columns = data.columns[1:] #Getting all numerical columns after first column
+        numerical_columns = data.columns[1:] # Getting all numerical columns after first column
         data[numerical_columns] = data[numerical_columns].apply(pd.to_numeric, errors='coerce') # make everything numeric
         
         data.set_index('', inplace=True) # set first column as the index
         
-        print("*** first 5 rows of data loaded ***")
-        print(data.head())
+        if print_data: # if print_data=True it will print this. if False it wont.
+            print("*** first 5 rows of data loaded ***")
+            print(data.head())
         
         return data
-        
+      
     def parseData(self, year_choice, region_choice):
         data = self.loadDataFile()
         
@@ -47,6 +48,7 @@ class VisitorsAnalyticsUtils:
             4: (2008, 2017)
         }
         
+        # getting start of year and end of year from the selected
         start_year, end_year = periods[year_choice]
         
         data['year'] = data.index.to_series().apply(lambda x: int(x.split()[0])) #exract year from first column
@@ -54,8 +56,8 @@ class VisitorsAnalyticsUtils:
         data = data[(data['year'] >= start_year) & (data['year'] <= end_year)]
         data = data[selected_region + ['year']]
         
-        for column in selected_region:
-            data[column] = data[column].astype('int64')
+        for column in selected_region: 
+            data[column] = data[column].astype('int64') # converting all columns to int64 type
         
         print("\n*** Parsed Data (Regions) ***")
         print(data.info())
@@ -66,15 +68,15 @@ class VisitorsAnalyticsUtils:
         return data
 
     def getTop3Countries(self, data):
+        # drop the 'year' column, sum each column, sort the values in descending order,
+        # and select the top 3 countries
         top_countries = data.drop(columns=['year']).sum().sort_values(ascending=False).head(3)
         
         print("\n*** Top 3 Countries ***")
         print(top_countries)
         return top_countries
 
-def main():
-    year_choice = 0
-    region_choice = 0
+if __name__ == '__main__':
     while True:
         try:
             year_choice = int(input("Enter year period (1: 1978-1987, 2: 1988-1997, 3: 1998-2007, 4: 2008-2017): "))
@@ -87,15 +89,10 @@ def main():
 
             break
         except ValueError as ve:
-            print(ve)
+            print(ve) # accuratly prints the error
             continue
 
-    # Made it easier to read this way
     utils = VisitorsAnalyticsUtils()
     parsed_data = utils.parseData(year_choice, region_choice)
     utils.getTop3Countries(parsed_data)
-
-    
-if __name__ == '__main__':
-    main()
 
